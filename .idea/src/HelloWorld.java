1f9a984d518528465edfaf850cce768e638d9342
arun sandapeta <arunkumarsak7642@gmail.com>
2024-09-21 16:02:51 +0530

    commited HelloWorld

--- .idea/src/HelloWorld.java
+++ .idea/src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String args[]){
-        System.out.println("Hello fixed changes with bugd");
+        System.out.println("Hello fixed changes with bugs");
     }
 }
